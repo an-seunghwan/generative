15c491eccb002f0542142fa3929c0ef31c80621a
an-seunghwan <dpeltms79@gmail.com>
2021-06-01 16:58:23 +0900

    update

--- ncsn/src/train_toy.py
+++ ncsn/src/train_toy.py
@@ -29,6 +29,7 @@ PARAMS = {
     "sigma_high":20.0
 }
 #%%
+'''true data distribution (Gaussian mixture)'''
 import tensorflow_probability as tfp
 tfd = tfp.distributions
 
@@ -40,14 +41,14 @@ def gmm(probs, loc, scale):
             scale_identity_multiplier=scale))
     return gmm
 
+def sample(gmm, sample_shape):
+    s = tfd.Sample(gmm, sample_shape=sample_shape)
+    return s.sample()
+
 mixture_prob = [0.2, 0.8]
 mean_parameters = [[-5, -5], [5, 5]]
 variance_parameters = [1, 1]
 gmm = gmm(mixture_prob, mean_parameters, variance_parameters)
-#%%
-def sample(gmm, sample_shape):
-    s = tfd.Sample(gmm, sample_shape=sample_shape)
-    return s.sample()
 #%%
 '''scorenet'''
 class ModelMLP(K.Model):
@@ -67,15 +68,15 @@ class ModelMLP(K.Model):
         x = self.mlp3(x)
         return x
 #%%
-'''sliced score matching'''
+'''loss of sliced score matching'''
 @tf.function
 def ssm_loss(scorenet, x_batch):
     sum_over = list(range(1, len(x_batch.shape)))
-    v = tf.random.normal(x_batch.shape)
+    v = tf.random.normal(x_batch.shape) # random vector follows Guassian
     with tf.GradientTape() as tape:
         tape.watch(x_batch)
         scores = scorenet(x_batch)
-        v_times_scores = tf.reduce_sum(v * scores) # v^\top * s(x) -> scalar
+        v_times_scores = tf.reduce_sum(v * scores) # v^\top * s(x) (scalar)
     grad = tape.gradient(v_times_scores, x_batch) # d/dx (v^\top * s(x)) = v^\top * (d/dx s(x))
     loss_first = tf.reduce_sum(grad * v, axis=sum_over) # v^\top * (d/dx s(x)) * v
     loss_second = 0.5 * tf.reduce_sum(tf.square(scores), axis=sum_over)
@@ -90,7 +91,6 @@ progress_bar = tqdm(range(PARAMS['epochs']))
 progress_bar.set_description('iteration {}/{} | current loss ?'.format(step, PARAMS['epochs']))
 #%%
 '''training'''
-avg_loss = 0
 for _ in progress_bar:
     x_batch = sample(gmm, PARAMS['batch_size'])
     step += 1
@@ -104,14 +104,12 @@ for _ in progress_bar:
         step, PARAMS['epochs'], current_loss
     ))
 
-    avg_loss += current_loss
-    
     if step == PARAMS['epochs']: break
 #%%
 @tf.function
 def analytic_log_gmm_prob_grad(x, sigma_i):
     '''
-    ground truth gradient of gmm
+    ground truth gradient data score of gmm
     d/dx log gmm 
     = d/dx log (\pi_1 normal1 + \pi_2 normal2)
     = d/dx log exp (log \pi_1 + log normal1 + log \pi_2 + log normal2), where exp is element-wise
@@ -134,46 +132,23 @@ def analytic_log_gmm_prob_grad(x, sigma_i):
 def langevin_dynamics(grad_function, x, sigma_i=None, alpha=0.1, T=1000):
     for _ in range(T):
         score = grad_function(x, sigma_i)
-        noise = tf.sqrt(alpha) * tf.random.normal(shape=x.get_shape(), mean=0, stddev=1)
-        x = x + (alpha / 2) * score + noise
+        noise = tf.random.normal(shape=x.get_shape(), mean=0, stddev=1)
+        x = x + (alpha / 2) * score + tf.sqrt(alpha) * noise
     return x
 
 @tf.function
 def annealed_langevin_dynamics(grad_function, x, sigma_levels, T=100, eps=0.1):
     for sigma_i in sigma_levels:
         alpha_i = eps * (sigma_i ** 2) / (sigma_levels[-1] ** 2) # step size
-        x = langevin_dynamics(grad_function, x, sigma_i=sigma_i, alpha=alpha_i, T=T)
+        x = langevin_dynamics(grad_function, x, sigma_i=sigma_i, alpha=alpha_i, T=T) # Langevin dynamics
     return x
-#%%
-# geometric sequence of sigma
-sigma_levels = tf.math.exp(tf.linspace(tf.math.log(PARAMS['sigma_high']),
-                                        tf.math.log(PARAMS['sigma_low']),
-                                        PARAMS['num_L']))
-#%%
-epsilon = 0.1
-x_init = tf.random.uniform(shape=(1280, 2), minval=-8, maxval=8)
-'''
-analytic_log_prob_grad: ground truth data score of gmm
-'''
-samples = annealed_langevin_dynamics(analytic_log_gmm_prob_grad, x_init, sigma_levels, T=100, eps=epsilon)
-plt.scatter(samples.numpy()[:, 0], samples.numpy()[:, 1], s=0.5, marker='.', color='black')
-#%%
+
 def meshgrid(x):
     y = x
     [gx, gy] = np.meshgrid(x, y, indexing='ij')
     gx, gy = np.float32(gx), np.float32(gy)
     grid = np.concatenate([gx.ravel()[None, :], gy.ravel()[None, :]], axis=0)
     return grid.T.reshape(x.size, y.size, 2)
-
-def visualize_density(gmm, x):
-    grid = meshgrid(x)
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax.imshow(gmm.prob(grid), cmap='inferno', extent=[-8, 8, -8, 8], origin='lower')
-    ax.set_xlabel(r'$x$')
-    ax.set_ylabel(r'$y$')
-    # plt.savefig("density.pdf", bbox_inches="tight")
-    plt.show()
-    return
 
 def visualize_gradients(x, grads, filename="gradients"):
     U, V = grads[:, :, 1], grads[:, :, 0]
@@ -186,21 +161,46 @@ def visualize_gradients(x, grads, filename="gradients"):
     plt.show()
     return
 #%%
-# define grids
+'''geometric sequence of sigma'''
+sigma_levels = tf.math.exp(tf.linspace(tf.math.log(PARAMS['sigma_high']),
+                                        tf.math.log(PARAMS['sigma_low']),
+                                        PARAMS['num_L']))
+
+epsilon = 0.1
+x_init = tf.random.uniform(shape=(1280, 2), minval=-8, maxval=8)
+samples = annealed_langevin_dynamics(analytic_log_gmm_prob_grad, x_init, sigma_levels, T=100, eps=epsilon)
+#%%
+'''plot density and generated samples'''
 x = np.linspace(-8, 8, 500, dtype=np.float32)
+fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+axes.flatten()[0].imshow(gmm.prob(meshgrid(x)), cmap='inferno', extent=[-8, 8, -8, 8], origin='lower')
+axes.flatten()[0].set_xlabel(r'$x$')
+axes.flatten()[0].set_ylabel(r'$y$')
+axes.flatten()[0].set_title('i.i.d samples')
+axes.flatten()[1].scatter(samples.numpy()[:, 0], samples.numpy()[:, 1], s=0.5, marker='.', color='black')
+axes.flatten()[1].set_title('annealed Langevin dynamics samples')
+plt.savefig('./ncsn/assets/score.png', bbox_inches="tight")
+plt.show()
+#%%
+'''true score vs estimated score'''
 x_for_grads = np.linspace(-8, 8, num=20)
-
-# plot density
-visualize_density(gmm, x)
-
-# compute analytic gradients
 grid = meshgrid(x_for_grads)
-true_grads = analytic_log_gmm_prob_grad(grid, 1)
+true_grads = analytic_log_gmm_prob_grad(grid, 1) # compute analytic gradients
+estimated_grads = model(grid) # compute estimated gradients (scores)
 
-# compute estimated gradients (scores)
-estimated_grads = model(grid)
+U1, V1 = true_grads[:, :, 1], true_grads[:, :, 0]
+U2, V2 = estimated_grads[:, :, 1], estimated_grads[:, :, 0]
 
-# visualize gradients
-visualize_gradients(x_for_grads, true_grads, "grad_analytic")
-visualize_gradients(x_for_grads, estimated_grads, "grad_est")
+fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+axes.flatten()[0].quiver(x_for_grads, x_for_grads, U1, V1)
+axes.flatten()[0].set_xlabel(r'$x$')
+axes.flatten()[0].set_ylabel(r'$y$')
+axes.flatten()[0].set_title('data scores')
+axes.flatten()[1].quiver(x_for_grads, x_for_grads, U2, V2)
+axes.flatten()[1].set_xlabel(r'$x$')
+axes.flatten()[1].set_ylabel(r'$y$')
+axes.flatten()[1].set_title('estimated scores')
+plt.gca().set_aspect('equal', adjustable='box')
+plt.savefig('./ncsn/assets/samples.png', bbox_inches="tight")
+plt.show()
 #%%

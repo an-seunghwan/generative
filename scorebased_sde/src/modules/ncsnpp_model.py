8fca98dfb7fd1d660aee59f9c32d8b03397b78a6
an-seunghwan <dpeltms79@gmail.com>
2021-07-02 21:05:28 +0900

    update

---
+++ scorebased_sde/src/modules/ncsnpp_model.py
@@ -0,0 +1,305 @@
+#%%
+# FIXME: layer normalization -> group normalization
+#%%
+import tensorflow as tf
+import tensorflow.keras as K
+from tensorflow.keras import layers
+import numpy as np
+#%%
+class Upsampling(layers.Layer):
+    def __init__(self, with_conv, in_ch=None):
+        super(Upsampling, self).__init__()
+        
+        self.with_conv = with_conv
+        if self.with_conv:
+            self.in_ch = in_ch
+            self.conv = layers.Conv2D(filters=self.in_ch, kernel_size=3, strides=1, padding='same', name='conv_up')
+
+    def call(self, x, **kwargs):
+        B, H, W, C = x.shape
+        x = tf.image.resize(x, size=[H * 2, W * 2], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        # assert x.shape == [B, H * 2, W * 2, C]
+        if self.with_conv:
+            x = self.conv(x)
+            # assert x.shape == [B, H * 2, W * 2, C]
+        return x    
+#%%
+class Downsampling(layers.Layer):
+    def __init__(self, with_conv, in_ch=None):
+        super(Downsampling, self).__init__()
+        
+        self.with_conv = with_conv
+        if self.with_conv:
+            self.in_ch = in_ch
+            self.conv = layers.Conv2D(filters=self.in_ch, kernel_size=3, strides=2, padding='same', name='conv_down')
+        else:
+            self.avgpool = layers.AveragePooling2D(pool_size=(2, 2), strides=2)
+
+    def call(self, x, **kwargs):
+        # B, H, W, C = x.shape
+        if self.with_conv:
+            x = self.conv(x)
+        else:
+            x = self.avgpool(x)
+        # assert x.shape == [B, H // 2, W // 2, C]
+        return x
+#%%
+def get_timestep_embedding(timesteps, embedding_dim):
+    """ 
+    From Fairseq.
+    Build sinusoidal embeddings.
+    This matches the implementation in tensor2tensor, but differs slightly
+    from the description in Section 3.5 of "Attention Is All You Need".
+    """
+    assert len(timesteps.shape) == 1  # and timesteps.dtype == tf.int32
+    
+    half_dim = embedding_dim // 2
+    emb = tf.math.log(10000.0) / (half_dim - 1)
+    emb = tf.exp(tf.range(half_dim, dtype=tf.float32) * -emb)
+    emb = tf.cast(timesteps, dtype=tf.float32)[:, None] * emb[None, :]
+    emb = tf.concat([tf.sin(emb), tf.cos(emb)], axis=1)
+    if embedding_dim % 2 == 1:  # zero pad
+        emb = tf.pad(emb, [[0, 0], [0, 1]])
+    # assert emb.shape == [timesteps.shape[0], embedding_dim]
+    return emb
+#%%
+# FIXME: Add Finite Impulse Response (FIR) Up or Down sampling 
+class ResnetBlock(layers.Layer):
+    def __init__(self, dropout, skip_rescale, in_ch, out_ch=None, up=False, down=False, scale_num=1):
+        """[summary]
+
+        Args:
+            dropout ([type]): [description]
+            skip_rescale ([type]): [description]
+            in_ch ([type]): [description]
+            out_ch ([type], optional): [description]. Defaults to None.
+            up (bool, optional): [description]. Defaults to False.
+            down (bool, optional): [description]. Defaults to False.
+            scale_num (int, optional): [description]. Defaults to 1.
+        """
+        super(ResnetBlock, self).__init__()
+        
+        self.up = up
+        self.down = down
+        self.dropout = dropout
+        self.skip_rescale = skip_rescale
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        if self.out_ch is None:
+            self.out_ch = self.in_ch
+        self.scale_num = scale_num
+        
+        if self.up:
+            self.shortcut = layers.Conv2DTranspose(filters=self.out_ch, kernel_size=1, strides=2 ** self.scale_num, padding='same', name='conv_shortcut')    
+        elif self.down:
+            self.shortcut = layers.Conv2D(filters=self.out_ch, kernel_size=1, strides=2 ** self.scale_num, padding='same', name='conv_shortcut')
+        elif self.out_ch != self.in_ch:
+            self.shortcut = layers.Conv2D(filters=self.out_ch, kernel_size=1, strides=1, padding='same', name='conv_shortcut')
+        
+        self.normalize1 = layers.LayerNormalization()
+        self.normalize2 = layers.LayerNormalization()
+        self.conv1 = layers.Conv2D(filters=self.out_ch, kernel_size=3, strides=1, padding='same', name='conv1')
+        self.temb_proj = layers.Dense(self.out_ch, name='temb_proj')
+        self.dropout_layer = layers.Dropout(rate=self.dropout)
+        self.conv2 = layers.Conv2D(filters=self.out_ch, kernel_size=3, strides=1, padding='same', name='conv2')
+
+    def call(self, x, temb, **kwargs):
+        B, H, W, C = x.shape
+        h = tf.nn.swish(self.normalize1(x))
+        
+        if self.up:
+            for _ in range(self.scale_num):
+                B, H, W, C = h.shape
+                h = tf.image.resize(h, size=[H * 2, W * 2], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        elif self.down:
+            for _ in range(self.scale_num):
+                B, H, W, C = h.shape
+                h = tf.image.resize(h, size=[H // 2, W // 2], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+            
+        h = self.conv1(h)
+
+        # add timestep embedding
+        h += self.temb_proj(tf.nn.swish(temb))[:, None, None, :]
+
+        h = tf.nn.swish(self.normalize2(h))
+        h = self.dropout_layer(h)
+        h = self.conv2(h)
+        
+        if self.up:
+            x = self.shortcut(x)
+        elif self.down:
+            x = self.shortcut(x)
+        elif self.out_ch != self.in_ch:
+            x = self.shortcut(x)
+        
+        # assert x.shape == h.shape
+        if self.skip_rescale:
+            return (x + h) / np.sqrt(2.)
+        else:
+            return x + h    
+#%%
+class AttentionBlock(layers.Layer):
+    def __init__(self, in_ch):
+        super(AttentionBlock, self).__init__()
+        
+        self.in_ch = in_ch
+        self.normalize = layers.LayerNormalization()
+        self.q_layer = layers.Dense(self.in_ch, name='q')
+        self.k_layer = layers.Dense(self.in_ch, name='k')
+        self.v_layer = layers.Dense(self.in_ch, name='v')
+        
+        self.proj_out = layers.Dense(self.in_ch, name='proj_out')
+
+    def call(self, x, **kwargs):
+        B, H, W, C = x.shape
+        h = self.normalize(x)
+        q = self.q_layer(h)
+        k = self.k_layer(h)
+        v = self.v_layer(h)
+        
+        w = tf.einsum('bhwc,bHWc->bhwHW', q, k) * (int(self.in_ch) ** (-0.5))
+        w = tf.reshape(w, [-1, H, W, H * W])
+        w = tf.nn.softmax(w, -1)
+        w = tf.reshape(w, [-1, H, W, H, W])
+        
+        h = tf.einsum('bhwHW,bHWc->bhwc', w, v)
+        h = self.proj_out(h)
+        
+        # assert h.shape == x.shape
+        return x + h
+#%%
+def build_unet(PARAMS):
+    
+    embedding_dim = PARAMS['embedding_dim']
+    skip_rescale = PARAMS['skip_rescale']
+    scale_num = PARAMS['scale_num']
+    dropout = PARAMS['dropout']
+    embedding_dim_mult = PARAMS['embedding_dim_mult']
+    num_res_blocks = PARAMS['num_res_blocks']
+    attn_resolutions = PARAMS['attn_resolutions']
+    progressive_input = PARAMS['progressive_input']
+    progressive_output = PARAMS['progressive_output']
+    progressive_combine = PARAMS['progressive_combine']
+    # attention_type = PARAMS['attention_type']
+    
+    x = layers.Input((PARAMS['data_dim'], PARAMS['data_dim'], PARAMS['channel']))
+    timesteps = layers.Input(())
+
+    num_resolutions = len(embedding_dim_mult)
+
+    '''Timestep embedding'''
+    temb = get_timestep_embedding(timesteps, embedding_dim)
+    temb = layers.Dense(embedding_dim * 4, name='dense0')(temb)
+    temb = layers.Dense(embedding_dim * 4, name='dense1')(tf.nn.swish(temb))
+    # assert temb.shape == [B, self.embedding_dim * 4]
+    
+    '''Downsampling'''
+    if progressive_input is not None:
+        input_pyramid = x
+        
+    hs = [layers.Conv2D(filters=embedding_dim, kernel_size=3, strides=1, padding='same', name='conv_in')(x)]
+    for i_level in range(num_resolutions):
+        # Residual blocks for this resolution
+        for i_block in range(num_res_blocks):
+            h = ResnetBlock(dropout=dropout, skip_rescale=skip_rescale, 
+                            in_ch=hs[-1].shape[-1], out_ch=embedding_dim * embedding_dim_mult[i_level], 
+                            up=False, down=False,
+                            scale_num=scale_num)(hs[-1], temb=temb)
+            
+            if h.shape[1] in attn_resolutions:
+                h = AttentionBlock(in_ch=h.shape[-1])(h)
+            hs.append(h)
+        # Downsample
+        if i_level != num_resolutions - 1:
+            h = ResnetBlock(dropout=dropout, skip_rescale=skip_rescale, 
+                            in_ch=hs[-1].shape[-1], out_ch=embedding_dim * embedding_dim_mult[i_level], 
+                            up=False, down=True,
+                            scale_num=scale_num)(hs[-1], temb=temb)
+            
+            if progressive_input == 'input_skip':
+                input_pyramid = Downsampling(with_conv=False, in_ch=None)(input_pyramid)
+                if progressive_combine == 'concat':
+                    h = layers.Concatenate(axis=-1)([input_pyramid, h])
+                elif progressive_combine == 'sum':
+                    h = input_pyramid = h
+
+            elif progressive_input == 'residual':
+                input_pyramid = Downsampling(with_conv=True, in_ch=h.shape[-1])(input_pyramid)
+                if skip_rescale:
+                    input_pyramid = (input_pyramid + h) / np.sqrt(2.)
+                else:
+                    input_pyramid = input_pyramid + h
+                h = input_pyramid
+            
+            hs.append(h)
+
+    '''Middle'''
+    h = hs[-1]
+    h = ResnetBlock(dropout=dropout, skip_rescale=skip_rescale, 
+                    in_ch=embedding_dim * embedding_dim_mult[-1], out_ch=None, 
+                    up=False, down=False,
+                    scale_num=scale_num)(h, temb=temb)
+    h = AttentionBlock(in_ch=h.shape[-1])(h)
+    h = ResnetBlock(dropout=dropout, skip_rescale=skip_rescale, 
+                    in_ch=embedding_dim * embedding_dim_mult[-1], out_ch=None, 
+                    up=False, down=False,
+                    scale_num=scale_num)(h, temb=temb)
+    
+    '''Upsampling'''
+    for i_level in reversed(range(num_resolutions)):
+        # Residual blocks for this resolution
+        for i_block in range(num_res_blocks + 1):
+            h = tf.concat([h, hs.pop()], axis=-1)
+            h = ResnetBlock(dropout=dropout, skip_rescale=skip_rescale, 
+                            in_ch=h.shape[-1], out_ch=embedding_dim * embedding_dim_mult[i_level], 
+                            up=False, down=False,
+                            scale_num=scale_num)(h, temb=temb)
+            
+            if h.shape[1] in attn_resolutions:
+                h = AttentionBlock(in_ch=h.shape[-1])(h)
+                
+        if progressive_output is not None:
+            if i_level == num_resolutions - 1:
+                if progressive_output == 'output_skip':
+                    pyramid = tf.nn.swish(layers.LayerNormalization()(h))
+                    pyramid = layers.Conv2D(filters=x.shape[-1], kernel_size=3, strides=1, padding='same')(pyramid)
+                    
+                elif progressive_output == 'residual':
+                    pyramid = tf.nn.swish(layers.LayerNormalization()(h))
+                    pyramid = layers.Conv2D(filters=h.shape[-1], kernel_size=3, strides=1, padding='same')(pyramid)
+            else:
+                if progressive_output == 'output_skip':
+                    pyramid = Upsampling(with_conv=False, in_ch=None)(pyramid)
+                    pyramid = tf.nn.swish(layers.LayerNormalization()(h))
+                    pyramid = pyramid + layers.Conv2D(filters=x.shape[-1], kernel_size=3, strides=1, padding='same')(pyramid)
+                    
+                elif progressive_output == 'residual':
+                    pyramid = Upsampling(with_conv=True, in_ch=h.shape[-1])(pyramid)
+                    if skip_rescale:
+                        pyramid = (pyramid + h) / np.sqrt(2.)
+                    else:
+                        pyramid = pyramid + h
+                    h = pyramid
+                        
+        # Upsample
+        if i_level != 0:
+            h = ResnetBlock(dropout=dropout, skip_rescale=skip_rescale, 
+                            in_ch=h.shape[-1], out_ch=None, 
+                            up=True, down=False,
+                            scale_num=scale_num)(h, temb=temb)
+            
+    '''End'''
+    if progressive_output == 'output_skip':
+        h = pyramid
+    else:
+        h = tf.nn.swish(layers.LayerNormalization()(h))
+        h = layers.Conv2D(filters=PARAMS['channel'], kernel_size=3, strides=1, padding='same', name='conv_out')(h)
+        
+    # assert h.shape == x.shape[:3] + [self.out_ch]
+    
+    model = K.models.Model([x, timesteps], h)
+
+    model.summary()
+
+    return model
+#%%
---
+++ scorebased_sde/src/ncsnpp.py
@@ -0,0 +1,229 @@
+'''
+Score-Based Generative Modeling through Stochastic Differential Equations
+: NCSN++ (discretized)
+'''
+#%%
+import tensorflow as tf
+import tensorflow.keras as K
+print('TensorFlow version:', tf.__version__)
+print('Eager Execution Mode:', tf.executing_eagerly())
+print('available GPU:', tf.config.list_physical_devices('GPU'))
+from tensorflow.python.client import device_lib
+print('==========================================')
+print(device_lib.list_local_devices())
+# tf.debugging.set_log_device_placement(False)
+#%%
+from tqdm import tqdm
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+# os.chdir(r'D:/generative/scorebased_sde')
+os.chdir('/Users/anseunghwan/Documents/GitHub/generative/scorebased_sde')
+
+from modules import ncsnpp_model
+#%%
+PARAMS = {
+    "batch_size": 128,
+    "epochs": 200000, 
+    "learning_rate": 0.0002, 
+    "data": "cifar10", 
+    "T": 1000,
+    "sigma_0": 0.01,
+    "sigma_T": 50.0,
+    
+    'embedding_dim':128, 
+    'skip_rescale':True,
+    'scale_num':1,
+    'dropout':0.1, 
+    'embedding_dim_mult':(1, 2, 2, 2), 
+    'num_res_blocks':4, 
+    'attn_resolutions':(16, ), 
+    'progressive_input':'residual',
+    'progressive_output':None,
+    'progressive_combine':'sum',
+    'attention_type':'ddpm',
+}
+
+#     model.ema_rate = 0.999
+#%%
+if PARAMS['data'] == "cifar10":
+    classnames = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    classdict = {i:x for i,x in enumerate(classnames)}
+
+    (x_train, _), (_, _) = K.datasets.cifar10.load_data()
+    '''-1 ~ +1 scaling'''
+    x_train = (x_train.astype('float32') - 127.5) / 127.5
+    PARAMS["data_dim"] = x_train.shape[1]
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train)).shuffle(len(x_train), reshuffle_each_iteration=True).batch(PARAMS['batch_size'])
+    
+    PARAMS["channel"] = 3 # RGB
+    PARAMS["mode"] = "RGB"
+    
+elif PARAMS['data'] == "mnist":
+    (x_train, _), (_, _) = K.datasets.mnist.load_data()
+    paddings = [[0, 0],
+                [4, 0],
+                [4, 0],
+                [0, 0]]
+    x_train = tf.pad(x_train[..., tf.newaxis], paddings, "CONSTANT") # same with CIFAR-10 dataset image size
+    '''-1 ~ +1 scaling'''
+    x_train = (tf.cast(x_train, 'float32') - 127.5) / 127.5
+    PARAMS["data_dim"] = x_train.shape[1]
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train)).shuffle(len(x_train), reshuffle_each_iteration=True).batch(PARAMS['batch_size'])
+    
+    PARAMS["channel"] = 1 # grayscale
+    PARAMS["mode"] = "L"
+    
+else:
+    print('Invalid data type!')
+    assert 0 == 1
+#%%
+# geometric sequence of sigma
+sigma_levels = tf.math.exp(tf.linspace(tf.math.log(PARAMS['sigma_0']),
+                                        tf.math.log(PARAMS['sigma_T']),
+                                        PARAMS['T']))
+#%%
+step = 0
+progress_bar = tqdm(range(PARAMS['epochs']))
+progress_bar.set_description('iteration {}/{} | current loss ?'.format(step, PARAMS['epochs']))
+#%%
+model = ncsnpp_model.build_unet(PARAMS)
+optimizer = K.optimizers.Adam(learning_rate=PARAMS['learning_rate'])
+
+@tf.function
+def train_one_step(optimizer, x_batch, x_batch_perturbed, noise, sigmas, timesteps):
+    with tf.GradientTape() as tape:
+        score = model([x_batch_perturbed, timesteps])
+        target = - noise / sigmas[:, tf.newaxis, tf.newaxis, tf.newaxis] ** 2
+        loss = tf.reduce_mean(tf.reduce_sum(tf.square(score - target), axis=[1,2,3])) * (sigmas ** 2)
+        gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return loss
+#%%
+'''training'''
+loss_history = []
+for _ in progress_bar:
+    x_batch = next(iter(train_dataset))
+    step += 1
+    
+    # sampling timestep
+    timesteps = tf.random.uniform([x_batch.shape[0]], 
+                                    minval=0,
+                                    maxval=PARAMS['T'],
+                                    dtype=tf.dtypes.int32)
+    sigmas = tf.gather(sigma_levels, timesteps)
+    epsilon = tf.random.normal(shape=x_batch.shape)
+    noise = sigmas[:, tf.newaxis, tf.newaxis, tf.newaxis] * epsilon
+    x_batch_perturbed = x_batch + noise # reparmetrization trick
+    
+    current_loss = train_one_step(optimizer, x_batch, x_batch_perturbed, noise, sigmas, timesteps)
+    loss_history.append(current_loss.numpy())
+    
+    progress_bar.set_description('setting: {} epochs:{} lr:{} dim:{} T:{} sigma:{} to {} | iteration {}/{} | current loss {:.3f}'.format(
+        PARAMS['data'], PARAMS['epochs'], PARAMS['learning_rate'], PARAMS['embedding_dim'], PARAMS['T'], PARAMS['sigma_0'], PARAMS['sigma_T'], 
+        step, PARAMS['epochs'], current_loss
+    ))
+
+    if step == PARAMS['epochs']: break
+#%%
+fig, ax = plt.subplots(figsize=(9, 4))
+ax.plot(loss_history)
+ax.set_title('loss')
+plt.savefig('./assets/loss_{}_{}_{}_{}_{}_{}.png'.format(PARAMS['data'], 
+                                                        PARAMS['learning_rate'], 
+                                                        PARAMS['embedding_dim'],
+                                                        PARAMS['T'],
+                                                        PARAMS['sigma_0'],
+                                                        PARAMS['sigma_T']))
+# plt.show()
+plt.close()
+#%%
+model.save_weights('./assets/{}/weights_{}_{}_{}_{}_{}_{}/weights'.format(PARAMS['data'],
+                                                                        PARAMS['data'], 
+                                                                        PARAMS['learning_rate'], 
+                                                                        PARAMS['embedding_dim'],
+                                                                        PARAMS['T'],
+                                                                        PARAMS['sigma_0'],
+                                                                        PARAMS['sigma_T']))
+#%%
+# model = ncsnpp_model.build_unet(PARAMS)
+# model.load_weights('./assets/{}/weights_{}_{}_{}_{}_{}_{}/weights'.format(PARAMS['data'],
+#                                                                         PARAMS['data'], 
+#                                                                         PARAMS['learning_rate'], 
+#                                                                         PARAMS['embedding_dim'],
+#                                                                         PARAMS['T'],
+#                                                                         PARAMS['sigma_0'],
+#                                                                         PARAMS['sigma_T']))
+#%%
+'''sampling'''
+def reverse_process(model, PARAMS, B, T=None, intermediate=False):
+    x = tf.random.normal(shape=[B, PARAMS['data_dim'], PARAMS['data_dim'], PARAMS['channel']], mean=0, stddev=1)
+    if intermediate:
+        x_list = []
+        
+        for t in tqdm(reversed(range(1, T))):
+            score = model([x, np.ones((B, )) * t])
+            x = x + (sigma_levels[t] - sigma_levels[t-1]) * score 
+            x = x + np.sqrt(sigma_levels[t-1] * (sigma_levels[t] - sigma_levels[t-1]) / sigma_levels[t]) * tf.random.normal(shape=x.get_shape(), mean=0, stddev=1)
+            x_list.append(x)
+            
+        score = model([x, np.zeros((B, ))])
+        x = x + sigma_levels[0] * score 
+        
+        return x_list
+    else:
+        for t in tqdm(reversed(range(1, T))):
+            score = model([x, np.ones((B, )) * t])
+            x = x + (sigma_levels[t] - sigma_levels[t-1]) * score 
+            x = x + np.sqrt(sigma_levels[t-1] * (sigma_levels[t] - sigma_levels[t-1]) / sigma_levels[t]) * tf.random.normal(shape=x.get_shape(), mean=0, stddev=1)
+            
+        score = model([x, np.zeros((B, ))])
+        x = x + sigma_levels[0] * score 
+        
+        return x
+#%%
+@tf.function
+def preprocess_image_to_save(x):
+    x = tf.clip_by_value(x, 0, 1)
+    x = x * 255
+    x = x + 0.5
+    x = tf.clip_by_value(x, 0, 255)
+    return x
+
+def save_as_grid(images, filename, spacing=2):
+    """
+    Partially from https://stackoverflow.com/questions/42040747/more-idiomatic-way-to-display-images-in-a-grid-with-numpy
+    :param images:
+    :return:
+    """
+    # Define grid dimensions
+    cols, rows, height, width, channels = images.shape
+
+    # Process image (clip values 0 ~ 1)
+    images = preprocess_image_to_save(images)
+
+    # Init image
+    grid_cols = rows * height + (rows + 1) * spacing
+    grid_rows = cols * width + (cols + 1) * spacing
+    im = Image.new(PARAMS['mode'], (grid_rows, grid_cols))
+    for row in range(rows):
+        for col in range(cols):
+            x = col * height + (1 + col) * spacing
+            y = row * width + (1 + row) * spacing
+            im.paste(tf.keras.preprocessing.image.array_to_img(images[col, row]), (x, y))
+    plt.axis('off')
+    plt.imshow(im)
+    plt.savefig('./assets/{}.png'.format(filename), bbox_inches="tight")
+    plt.close()
+#%%
+'''1. generating'''
+tf.random.set_seed(520)
+x = reverse_process(model, PARAMS, B=10, T=PARAMS['T'], intermediate=True)
+save_as_grid(x[None, ...], '{}_samples_{}_{}_{}_{}_{}'.format(PARAMS['data'], 
+                                                            PARAMS['learning_rate'], 
+                                                            PARAMS['embedding_dim'],
+                                                            PARAMS['T'],
+                                                            PARAMS['sigma_0'],
+                                                            PARAMS['sigma_T']))
+#%%
